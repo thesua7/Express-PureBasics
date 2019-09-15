cb97bba6bf4e6cc53a9a3f01b738427b957cf0f2
Sani Ahamed <thesua711@gmail.com>
2019-09-16 02:53:46 +0600

    Indrocution/Basics Completed
    Installing express,path function,static function,send respones of html/css/js

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Express Advance
+# Express Advance 
+Basics: 
+-- Difference between nodeJs and Express
+-- Some basics routing
+-- Sending responses of html=>css/js
+
+
  
--- index.js
+++ index.js
@@ -1,35 +1,110 @@
-const http = require('http')
-//Require means selecting the 'http' package 
+const express = require('express');
+const path = require('path');
+const app = express();
 
-const server = http.createServer((request,response)=>{
-//creating a server
-//() => {} callback function
-console.log(request.url);
 
-if(request.url === '/about'){
-    // "===" means strictly equal EX: ("1" === 1) -> expected output: false
+app.use(express.static('public'));
+//public folder for assets must run first 
 
-   return response.end('The about page');
-}
 
-else if(request.url === '/contact'){
-    return response.end('The contact page');
-}
 
-else if(request.url === '/'){
-    return response.end('The home page');
-}
-
-else{
-    response.writeHead(404);
-    //The persar going to response 404 for our server
+app.get('/',(request,response)=>{
+//First parameter is path and Second is what's going to execute on that path
     
-     response.end('The page was not found');
-     //The End method causes the Web server to stop processing the script and return the current result. 
-     //The remaining contents of the file are not processed.
-}
+response.sendFile(path.resolve(__dirname,'index.html'));
+/* .sendFile can't send directly path like index.html
+    so use path lib .resolve(__dirname(current directory),'the file name')
+*/
 
+
+// response.json({
+//     name: 'Sani Ahamed'
+// })
+//.json can send json file
 
 })
 
-server.listen(3000);
+
+app.get('/about',(request,response)=>{
+
+    response.sendFile(path.resolve(__dirname,'about.html'));
+
+
+    // response.send({
+    //     name: 'Sani Ahamed'
+    // })
+    //.send function can send anything
+})
+
+
+app.get('/contact',(request,response)=>{
+    
+    response.sendFile(path.resolve(__dirname,'contact.html'));
+})
+
+
+app.listen(3000,()=>{
+    console.log('App listening on 3000');
+    //Second parameter is callback function
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const http = require('http')
+// //Require means selecting the 'http' package 
+
+// const fs = require('fs');
+// //fs = file system
+
+// const aboutPage = fs.readFileSync('about.html');
+// //sync means synchroous / non blocking
+// const contactPage = fs.readFileSync('contact.html');
+
+// const homePage = fs.readFileSync('index.html');
+
+
+
+
+// const server = http.createServer((request,response)=>{
+// //creating a server
+// //() => {} callback function
+// console.log(request.url);
+
+// if(request.url === '/about'){
+//     // "===" means strictly equal EX: ("1" === 1) -> expected output: false
+
+//    return response.end(aboutPage);
+// }
+
+// else if(request.url === '/contact'){
+//     return response.end(contactPage);
+// }
+
+// else if(request.url === '/'){
+//     return response.end(homePage);
+// }
+
+// else{
+//     response.writeHead(404);
+//     //The persar going to response 404 for our server
+    
+//      response.end('The page was not found');
+//      //The End method causes the Web server to stop processing the script and return the current result. 
+//      //The remaining contents of the file are not processed.
+// }
+
+
+// })
+
+// server.listen(3000);
